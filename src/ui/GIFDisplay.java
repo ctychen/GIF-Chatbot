6433347cc5815366ccc3d9d098a2cf130b69ce9e
C T Chen <34198336+the-null-terminator@users.noreply.github.com>
2019-05-27 18:08:34 -0700

    Preparing for final

--- src/ui/GIFDisplay.java
+++ src/ui/GIFDisplay.java
@@ -13,7 +13,7 @@ public class GIFDisplay extends PApplet {
 	private static Meme fancyMeme = null; 
 	public static String memeURL = null;
 	public static String memeWebURL = null;
-	private PImage[] shelbys = new PImage[5];
+	private PImage[] shelbys = new PImage[6];
 	public static int shelbyIndex = 2;
 	public static boolean scaling = true;
 	
@@ -28,7 +28,7 @@ public class GIFDisplay extends PApplet {
 	
 	public void draw() {
 		background(255);
-		if (shelbyIndex == 1 || shelbyIndex == 3) {
+		if (shelbyIndex == 1 || shelbyIndex == 3 || shelbyIndex == 5) {
 			image(shelbys[shelbyIndex], 0, 0, width, height);
 			translate(width*0.5f, height*0.5f);
 			rotate(-0.02f*(float)Math.PI);
@@ -46,7 +46,15 @@ public class GIFDisplay extends PApplet {
 			text("Command Mode", width*0.2f+(int)(Math.random()*10), height-30-(int)(Math.random()*10));
 			
 		}
-		if (shelbyIndex != 1 && shelbyIndex != 0 && shelbyIndex != 3) {
+		
+		if (shelbyIndex == 5) {
+			textSize(width*(float)Math.random()*0.03f+0.05f*width);
+			translate(-width*0.1f, -height*0.1f);
+			fill((int)(Math.random()*255), (int)(Math.random()*255), (int)(Math.random()*255));
+			text("Listening", width*0.2f+(int)(Math.random()*10), height-30-(int)(Math.random()*10));
+			
+		}
+		if (shelbyIndex != 1 && shelbyIndex != 0 && shelbyIndex != 3 && shelbyIndex != 5) {
 			if (memeURL != null) {
 				fancyMeme = new Meme(memeURL, this);
 				fancyMeme.setWebURL(memeWebURL);
